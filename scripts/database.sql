de870289969d62793db1d2b5b704820afbb542a8
Nansarpong <96475937+Nansarpong@users.noreply.github.com>
2026-05-22 12:44:18 +0100

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -11,8 +11,14 @@ WARNING:
     Running this script will drop the entire 'HOPEDataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
+
+Extra warning - please note for this project and because of security limitations we will be using an already created database (WCC_Reports_Live) that engineers at westminster can use 
+to test create their pipelines. as such there are a few modifications you will see below on script blocked out and another script that i had to use due to the limitations of using 
+the WCC_Reports_Live database.  
 */
 
+/* 
+-- this is the original script that i created which would work if  had access to the master database 
 USE master;
 GO
 
@@ -30,13 +36,16 @@ GO
 
 USE HOPEDataWarehouse;
 GO
+*/
 
+
+-- this is the script to create the schemas in the (WCC_Reports_Live) database
 -- Create Schemas
-CREATE SCHEMA bronze;
-GO
+use WCC_Reports_Live;
 
-CREATE SCHEMA silver;
+Create Schema HOPE_Bronze;
 GO
-
-CREATE SCHEMA gold;
+Create Schema HOPE_Silver;
+GO
+Create Schema HOPE_Gold;
 GO
