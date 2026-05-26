f4cb679cd2cc4b16588903a4561f716b848f0bfc
Nansarpong <96475937+Nansarpong@users.noreply.github.com>
2026-05-26 09:31:36 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -5,7 +5,7 @@ stored procedure: Load Bronze Layer ( source -. Bronze)
 script purpose: 
   THIS STORED PROCEDURE LOADS DATA INTO THE 'BRONZE' SCHEMA FROM EXTERNAL EXCEL/CSV FILES
   IT PERFORMS THE FOLLOWING ACTIONS: 
-  -Truncates the bronze tables before oading data. 
+  -Truncates the bronze tables before loading data. 
   -Uses the 'Bulk Insert' command to load data from csv file bronze tables.
 
 Parameters: 
@@ -40,8 +40,9 @@ BEGIN
 	
 		BULK INSERT [bronze].[HopeExtract]
 
--- get the path for below. 
-		FROM ----SharePoint
+-- get the path for below. should be sharepoint file but for immeddiacy the file is linked to the sharepoint file via personal one-drive file. (Security issue to be resolved)
+			
+		FROM C:\Users\nsarpon\OneDrive - Office Shared Service\General - Data & Performance - Housing Needs\Hopepipeline\HopeExtract.csv
 		WITH (
 		FirstROW = 2,
 		FIELDTERMINATOR = ',',
